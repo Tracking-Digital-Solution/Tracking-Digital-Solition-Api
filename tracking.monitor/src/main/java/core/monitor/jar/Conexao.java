f762c1cccbc7fee4f6a56567ad3c79d2ef9282ab
Isabela Saori <110927856+isabelasaorinakano@users.noreply.github.com>
2023-04-23 16:17:25 -0300

    jar + tracking monitor

--- tracking.monitor/src/main/java/core/monitor/jar/Conexao.java
+++ tracking.monitor/src/main/java/core/monitor/jar/Conexao.java
@@ -1,25 +1,15 @@
-package core.monitor.resources;
+package login.jar;
 
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.jdbc.core.JdbcTemplate;
+import com.microsoft.sqlserver.jdbc.SQLServerDriver;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
-/**
- * @author consultor
- */
-public class ConexaoService {
+public class Conexao {
 
     private JdbcTemplate conexaoDoBanco;
 
     //Configurando a conexão e o acesso
-
-    /**
-     *
-     */
-    public ConexaoService() {
+    public Conexao() {
         BasicDataSource dataSource = new BasicDataSource();
         dataSource.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
         dataSource​.setUrl("jdbc:sqlserver://bd-tracking-digital-solution.database.windows.net:1433;database=trackingdigitalsolution;encrypt"
@@ -28,20 +18,9 @@ public class ConexaoService {
         dataSource​.setPassword("#Gfgrupo10");
 
         this.conexaoDoBanco = new JdbcTemplate(dataSource);
-
     }
-
-
 
     public JdbcTemplate getConexaoDoBanco() {
         return conexaoDoBanco;
     }
-
-    public Connection driverManager() throws SQLException {
-        return DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/trackingdigitalsolution",
-                "grupo10user",
-                "grupo10user");
-    }
 }
-
---
+++ tracking.monitor/src/main/java/core/monitor/jar/Login.java
@@ -0,0 +1,201 @@
+package core.monitor.jar;
+
+import core.monitor.resources.ConexaoService;
+import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
+import login.jar.Conexao;
+import login.jar.Home;
+import login.jar.Usuario;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class Login extends javax.swing.JFrame {
+
+    Scanner leitor = new Scanner(System.in);
+    ConexaoService conexao = new ConexaoService();
+    JdbcTemplate con = conexao.getConexaoDoBanco();
+
+    public Login() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        ipt_senha = new javax.swing.JTextField();
+        ipt_email = new javax.swing.JTextField();
+        btn_entrar = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setText("LOGIN");
+
+        jLabel2.setText("Email");
+
+        jLabel3.setText("Senha");
+
+        ipt_senha.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ipt_senhaActionPerformed(evt);
+            }
+        });
+
+        ipt_email.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ipt_emailActionPerformed(evt);
+            }
+        });
+
+        btn_entrar.setText("ENTRAR");
+        btn_entrar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_entrarActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(175, 175, 175)
+                        .addComponent(jLabel1))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(29, 29, 29)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel3)
+                            .addComponent(jLabel2)
+                            .addComponent(ipt_senha, javax.swing.GroupLayout.PREFERRED_SIZE, 235, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(ipt_email, javax.swing.GroupLayout.PREFERRED_SIZE, 235, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(159, 159, 159)
+                        .addComponent(btn_entrar)))
+                .addContainerGap(136, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1)
+                .addGap(40, 40, 40)
+                .addComponent(jLabel2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(ipt_email, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(33, 33, 33)
+                .addComponent(jLabel3)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(ipt_senha, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 36, Short.MAX_VALUE)
+                .addComponent(btn_entrar)
+                .addGap(19, 19, 19))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+    private void ipt_emailActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ipt_emailActionPerformed
+        // TODO add your handling code here:
+        email = ipt_email.getText();
+        System.out.println(email);
+
+    }//GEN-LAST:event_ipt_emailActionPerformed
+
+    String email;
+    private void btn_entrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_entrarActionPerformed
+        // TODO add your handling code here:
+        email = ipt_email.getText();
+        senha = ipt_senha.getText();
+        System.out.println(senha);
+        System.out.println(email);
+
+//        for (int i = 0;
+//                i < usuariosDoBanco.size();
+//                i++) {
+//            if (email.equals(usuariosDoBanco.get(i).getEmail())
+//                    && senha.equals(usuariosDoBanco.get(i).getSenha())) {
+//                new Home().setVisible(true);
+//            } else {
+//                System.out.println("Usuário ou senha incorretos");
+//                System.out.println(usuariosDoBanco.get(i).getSenha());
+//
+//            }
+        if (validationLogin()) {
+                new Home().setVisible(true);
+            //System.out.println("foi");
+        } else {
+            System.out.println("Erro ao cadastrar Login");
+        }
+    }//GEN-LAST:event_btn_entrarActionPerformed
+    String senha;
+    private void ipt_senhaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ipt_senhaActionPerformed
+        // TODO add your handling code here:
+        senha = ipt_senha.getText();
+    }//GEN-LAST:event_ipt_senhaActionPerformed
+
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Login.class
+                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Login.class
+                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Login.class
+                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Login.class
+                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Login().setVisible(true);
+            }
+        });
+    }
+
+    private Boolean validationLogin() {
+        
+        try {
+        con.queryForObject(String.format("select * from perfil where email = '%s' and senha = '%s'", email, senha),
+                new BeanPropertyRowMapper<>(Usuario.class));    
+        } catch(DataAccessException e) {
+            System.out.println(e.getMessage());
+            return false;
+             
+        }
+       
+        return true;
+
+    }
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btn_entrar;
+    private javax.swing.JTextField ipt_email;
+    private javax.swing.JTextField ipt_senha;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    // End of variables declaration//GEN-END:variables
+
+}
