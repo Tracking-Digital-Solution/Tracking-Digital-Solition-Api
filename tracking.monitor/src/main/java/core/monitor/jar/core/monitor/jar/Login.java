b2a9c5c3e660b57a13b51065adc316234d95efed
Gabriel Macedo Vilas Boas <gabriel.boas@sptech.school>
2023-05-15 19:09:24 -0300

    Resolve conflict

--- tracking.monitor/src/main/java/core/monitor/jar/core/monitor/jar/Login.java
+++ tracking.monitor/src/main/java/core/monitor/jar/core/monitor/jar/Login.java
@@ -134,7 +134,7 @@ public class Login extends javax.swing.JFrame {
         );
 
         jLabel1.setBackground(new java.awt.Color(3, 29, 68));
-        jLabel1.setIcon(new javax.swing.ImageIcon("C:\\Users\\gabsm\\Documents\\2º Semestre\\Pesquisa e Inovação\\Tracking-Digital-Solutions-Jar\\src\\main\\resources\\assets\\logo-tracking-monitor__1_-removebg-preview.png")); // NOI18N
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/logo-tracking-monitor__1_-removebg-preview.png"))); // NOI18N
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
